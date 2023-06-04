9948476352bf9f3c5ed4b9d3dcb982ca306338ac
watabun <126847386+watabun@users.noreply.github.com>
2023-06-05 08:54:09 +0900

    formbomitt

---
+++ zipCheckForm.php
@@ -0,0 +1,6 @@
+<form method="POST" action="zipCheck.php">
+    <ul>
+        <li><label>郵便番号：<input type="text" name="zip"></label></li>
+        <li><input type="submit" value="送信する"></li>
+    </ul>
+</form>
